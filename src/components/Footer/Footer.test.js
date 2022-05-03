1d29e5fe126e0979a057bc30a67e7f6af8896577
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-05-03 22:28:55 +0200

    test done

--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -1,10 +1,25 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { render, screen } from '@testing-library/react';
 import Footer from "./Footer";
+import {themeContext} from '../../context/themeContext';
 
 describe("Footer", () => {
+
+  const themeData = {
+    theme: "",
+    toggleTheme: () => theme === '' ? theme = "-dark" : theme = ""
+  }
+  test("footer text in the document", () => {
+    render(<themeContext.Provider value={themeData}>
+            <Footer />
+          </themeContext.Provider>);
+    const linkElement = screen.getByText(/Esto es el Footer/i);
+    expect(linkElement).toBeInTheDocument();
+  });
   test("matches snapshot", () => {
-    const wrapper = shallow(<Footer />);
-    expect(wrapper).toMatchSnapshot();
+    render(<themeContext.Provider value={themeData}>
+            <Footer />
+          </themeContext.Provider>);
+    expect(screen).toMatchSnapshot();
   });
 });
